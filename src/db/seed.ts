2a1b16e6c5171a485b09cc044f88e802cb907b40
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-13 02:59:33 -0300

    refactor: database seed to add goals completions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs'
+
 import { client, database } from '.'
 
 import { goalCompletions, goals } from './schema'
@@ -7,10 +9,23 @@ async function seed() {
 
   await database.delete(goals)
 
-  await database.insert(goals).values([
-    { title: 'Wake up early', desiredWeeklyFrequency: 5 },
-    { title: 'Workout', desiredWeeklyFrequency: 5 },
-    { title: 'Pray', desiredWeeklyFrequency: 7 },
+  const currentWeekGoals = await database
+    .insert(goals)
+    .values([
+      { title: 'Wake up early', desiredWeeklyFrequency: 5 },
+      { title: 'Workout', desiredWeeklyFrequency: 5 },
+      { title: 'Pray', desiredWeeklyFrequency: 7 },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await database.insert(goalCompletions).values([
+    { goalId: currentWeekGoals[0].id, createdAt: startOfWeek.toDate() },
+    {
+      goalId: currentWeekGoals[1].id,
+      createdAt: startOfWeek.add(1, 'day').toDate(),
+    },
   ])
 }
 
