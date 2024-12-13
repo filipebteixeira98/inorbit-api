52f23667467487311ec41cf8acec5d779ad1a5b3
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-13 20:38:58 -0300

    fix: imports of application routes

--- src/http/routes/create-completion.ts
+++ src/http/routes/create-completion.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
-import { createGoalCompletion } from '../functions/create-goal-completion'
+import { createGoalCompletion } from '../../functions/create-goal-completion'
 
 export const createCompletionRoute: FastifyPluginAsyncZod = async app => {
   app.post(
--- src/http/routes/create-goal.ts
+++ src/http/routes/create-goal.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
-import { createGoal } from '../functions/create-goal'
+import { createGoal } from '../../functions/create-goal'
 
 export const createGoalRoute: FastifyPluginAsyncZod = async app => {
   app.post(
