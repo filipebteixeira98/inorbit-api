30661b9d0c983a8d418e7684f5de846d93ef311a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-13 04:08:02 -0300

    feat: add functionality that allows users to create a goal

---
+++ src/functions/create-goal.ts
@@ -0,0 +1,27 @@
+import { database } from '../db'
+
+import { goals } from '../db/schema'
+
+interface CreateGoalRequest {
+  title: string
+  desiredWeeklyFrequency: number
+}
+
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest) {
+  const result = await database
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning()
+
+  const goal = result[0]
+
+  return {
+    goal,
+  }
+}
