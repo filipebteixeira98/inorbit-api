1c49a9f3f3dd6c7cdb917907f208e46a605c1109
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-13 02:08:20 -0300

    chore: create schema validation to orm

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
 
+import { env } from './src/env'
+
 export default defineConfig({
   schema: './schema.ts',
   out: './.migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: env.DATABASE_URL,
   },
 })
