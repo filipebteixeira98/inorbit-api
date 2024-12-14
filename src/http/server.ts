1adbaee4724ea5933bbc642a7737d0466c7ca653
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-13 23:59:11 -0300

    chore: configure @fastify/cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import fastifyCors from '@fastify/cors'
 
 import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
@@ -11,6 +12,10 @@ import { getPendingGoalsRoute } from './routes/get-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 
