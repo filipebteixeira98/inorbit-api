7c8098424031036ed45ecb3748b214c4d791514f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-20 03:56:23 -0300

    fix: error of instantiation of drizzle kit schema

--- .migrations/0000_melted_squadron_sinister.sql
+++ .migrations/0000_melted_squadron_sinister.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "goals" (
 	"id" text PRIMARY KEY NOT NULL,
-	"text" text NOT NULL,
+	"title" text NOT NULL,
 	"desired_weekly_frequency" integer NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL
 );
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -5,7 +5,7 @@ export const goals = pgTable('goals', {
   id: text('id')
     .primaryKey()
     .$defaultFn(() => createId()),
-  title: text('text').notNull(),
+  title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
