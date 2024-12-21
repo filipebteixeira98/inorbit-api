d7761b276246d8944b836342c60c51c80e8028da
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-21 08:41:07 -0300

    chore: get goals completed by week day in descending order

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -40,6 +40,7 @@ export async function getWeekSummary() {
           lte(goalCompletions.createdAt, lastDayOfWeek)
         )
       )
+      .orderBy(desc(goalCompletions.createdAt))
   )
 
   const goalsCompletedByWeekDay = database
