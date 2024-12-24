ae399ca5a6ab29a07cfbf925d2dfeb0ad7286184
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-12-24 14:54:42 -0300

    chore: configure github provider authentication

---
+++ src/functions/authenticate-from-github-code.ts
@@ -0,0 +1,47 @@
+import { eq } from 'drizzle-orm'
+
+import { database } from '../db'
+
+import { users } from '../db/schema'
+
+import {
+  getAccessTokenFromCode,
+  getUserFromAccessToken,
+} from '../modules/github-oauth'
+
+interface AuthenticateFromGithubCodeRequest {
+  code: string
+}
+
+export async function AuthenticateFromGithubCode({
+  code,
+}: AuthenticateFromGithubCodeRequest) {
+  const accessToken = await getAccessTokenFromCode(code)
+
+  const githubUser = await getUserFromAccessToken(accessToken)
+
+  const result = await database
+    .select()
+    .from(users)
+    .where(eq(users.externalAccountId, githubUser.id))
+
+  let userId: string | null
+
+  const userAlreadyExists = result.length > 0
+
+  if (userAlreadyExists) {
+    userId = result[0].id
+  } else {
+    const [insertedUser] = await database
+      .insert(users)
+      .values({
+        name: githubUser.name,
+        email: githubUser.email,
+        avatarUrl: githubUser.avatar_url,
+        externalAccountId: githubUser.id,
+      })
+      .returning()
+
+    userId = insertedUser.id
+  }
+}
