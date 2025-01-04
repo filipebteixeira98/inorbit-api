8d93b7629c30be9d68682b6e64c545703660db10
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-01-04 16:53:58 -0300

    fix: error adding authenticate user hook in remaining routes that need access to request sub property

--- src/http/routes/create-completion.ts
+++ src/http/routes/create-completion.ts
@@ -3,10 +3,13 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
 import { createGoalCompletion } from '../../functions/create-goal-completion'
 
+import { authenticateUserHook } from '../hooks/authenticate-user'
+
 export const createCompletionRoute: FastifyPluginAsyncZod = async app => {
   app.post(
     '/completions',
     {
+      onRequest: [authenticateUserHook],
       schema: {
         tags: ['goals'],
         description: 'Complete a goal',
